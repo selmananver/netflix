54a4b9bf49a4db8983588b1c6e129f30321abe3a
selmananver <selman.habteq@gmail.com>
2023-10-10 18:44:22 +0530

    Updated

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,17 +1,17 @@
-import React from 'react';
+// import React from 'react';
 
-const Auth = () => {
-    const auth = ( localStorage.getItem('user'));
+// const Auth = () => {
+//     const auth = ( localStorage.getItem('user'));
 
-    // If has token, return outlet in other case return navigate to login page
+//     // If has token, return outlet in other case return navigate to login page
 
-    if(auth ===null){
-        window.location.href ='/login';
-    }
-    else{
-      window.location.href ='/';
-    }
-    console.log(auth);
-}
+//     if(auth ===null){
+//         window.location.href ='/login';
+//     }
+//     else{
+//       window.location.href ='/';
+//     }
+//     console.log(auth);
+// }
 
-export default Auth
+// export default Auth
